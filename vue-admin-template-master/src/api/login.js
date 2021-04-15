78baa94366c45b2625a060fb527398968bda9b2b
MATT-021 <550369801@qq.com>
2021-04-15 10:25:13 +0800

    阿里云 对象oss

--- vue-admin-template-master/src/api/login.js
+++ vue-admin-template-master/src/api/login.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 export function login(username, password) {
   return request({
-    url: 'eduService/user/login',
+    url: 'edu/user/login',
     method: 'post',
     data: {
       username,
@@ -13,7 +13,7 @@ export function login(username, password) {
 
 export function getInfo(token) {
   return request({
-    url: 'eduService/user/info',
+    url: 'edu/user/info',
     method: 'get',
     params: { token }
   })
